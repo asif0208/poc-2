1c22f13d66774c2c4d6ee20a1d679f4ee87121d0
root <root@ip-172-31-22-8.ap-south-1.compute.internal>
2018-10-08 04:47:48 +0000

    this is the second attempt

---
+++ src/main/java/pocpackage/App.java
@@ -0,0 +1,13 @@
+package pocpackage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
